70eb51ef4e6d96cfbaa5164b412f7351a55fe066
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-05-24 21:16:12 +0200

    Update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 LIST_FILE_NAME="packages.txt"
 sudo apt-get update
-sudo apt-get install $(cat $LIST_FILE_NAME)
+sudo apt-get install -y $(cat $LIST_FILE_NAME)
 
 # OMF
 curl -L https://get.oh-my.fish | fish
