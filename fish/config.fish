70b97da4b206f971e5c215406e9b54ddcd579d72
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-05-25 00:46:28 +0200

    Some changes...

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ set fish_greeting ""
 alias vim="nvim"
 alias vi="nvim"
 alias lsl="ls -lthr"
+alias feh="feh --fullscreen"
 set -x SLACK_TOKEN 'xoxp-298979420741-298861719698-368285980582-5215c40567b6616d1f8cfa7730d3a34a'
 bash $HOME/.dotfiles/check_git.sh
