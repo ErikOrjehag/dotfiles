9564f9f19e60b1affc0aa62560341d4611f0a5ba
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-05-24 21:10:55 +0200

    Add google chrome to install

--- add_external.sh
+++ add_external.sh
@@ -9,5 +9,10 @@ sudo apt-add-repository ppa:fish-shell/release-2
 # Flux
 sudo add-apt-repository ppa:nathan-renniewaldock/flux
 
+# Google chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
 
 sudo apt-get update
