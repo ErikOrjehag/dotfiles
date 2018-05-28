0257c5753ddf9564497df0a96edac1f8921012b0
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-05-28 22:12:35 +0200

    Changed files for desktop setup

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,6 @@ alias vim="nvim"
 alias vi="nvim"
 alias lsl="ls -lthr"
 alias feh="feh --fullscreen"
+alias ts3="bash /home/alexander/packages/teamspeak3/ts3client_runscript.sh"
 set -x SLACK_TOKEN 'xoxp-298979420741-298861719698-368285980582-5215c40567b6616d1f8cfa7730d3a34a'
 bash $HOME/.dotfiles/check_git.sh
