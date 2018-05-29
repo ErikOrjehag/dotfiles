6a9f0305993bfc89a6ec0e24db64e68c06552879
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-05-29 13:02:24 +0200

    Add some more external packages

--- add_external.sh
+++ add_external.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+# Before install
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install curl
+
 # Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -19,5 +25,12 @@ rm googe-chrome*.deb
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# NodeJS
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+# Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update
