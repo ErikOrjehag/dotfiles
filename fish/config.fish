f425b3e807f6f7b1aa6fe4c983e895a63c405288
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-09-14 14:29:00 +0200

    Vscode and fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,6 @@ alias vim="nvim"
 alias vi="nvim"
 alias lsl="ls -lthr"
 alias feh="feh --fullscreen"
-set -x SLACK_TOKEN 'xoxp-298979420741-298861719698-368285980582-5215c40567b6616d1f8cfa7730d3a34a'
+alias g++17="g++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wsuggest-attribute=const -fconcepts"
+alias impa="g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE"
 bash $HOME/.dotfiles/check_git.sh
