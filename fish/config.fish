0336e3f700a99dbf12edf8da6b0127ef68532184
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-10-05 18:42:46 +0200

    asd

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,10 @@ alias feh="feh --fullscreen"
 alias g++17="g++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wsuggest-attribute=const -fconcepts"
 alias impa="g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE"
 bash $HOME/.dotfiles/check_git.sh
+
+
+
+function rar_play 
+    unar *.rar
+    vlc *.avi
+end
