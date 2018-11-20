26a306182cb444e1eba112629f3f537bb9893d9a
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-11-20 21:27:21 +0100

    Unclear

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,6 @@ alias lsl="ls -lthr"
 alias feh="feh --fullscreen"
 alias ts3="bash /home/alexander/packages/teamspeak3/ts3client_runscript.sh"
 bash $HOME/.dotfiles/check_git.sh
+alias g++17="g++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wsuggest-attribute=const -fconcepts"
+alias impa="g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE"
+alias i="sudo apt install "
